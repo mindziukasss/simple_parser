f1f014a842a9149f0a83fca48c21ca8d90582a25
mindziukasss <mindziukasss@gmail.com>
2017-02-07 15:55:33 +0200

    Added email search

--- app/controllers/email_controller.rb
+++ app/controllers/email_controller.rb
@@ -1,7 +1,12 @@
 class EmailController < ApplicationController
 
   def email_list
-    @emails = Email.all
+    @emails = Email.all.order("created_at DESC")
+    if params[:search]
+        @emails = Email.search(params[:search]).order("created_at DESC")
+      else
+        @email = Email.all.order("created_at DESC")
+      end
   end
 
 end
--- app/models/email.rb
+++ app/models/email.rb
@@ -1,4 +1,9 @@
 class Email < ApplicationRecord
   
 	validates_uniqueness_of :email
+
+  def self.search(search)
+    where("email LIKE ?", "%#{search}%") 
+  end
+
 end
